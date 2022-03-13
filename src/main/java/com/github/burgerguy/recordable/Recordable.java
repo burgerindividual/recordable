9eb3362c6e2f579658fb5ffb2ebd603f2eee84f1
burgerguy <ryan.gehling@yahoo.com>
2022-03-13 10:10:11 -0400

    Change package format, edit mod json

---
+++ src/main/java/com/github/burgerguy/recordable/Recordable.java
@@ -0,0 +1,18 @@
+package com.github.burgerguy.recordable;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Recordable implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("recordable");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
